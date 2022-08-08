31d8922a684589258339be5ac844f43d17aa1a2d
unknown <mitrovicbojan87@gmail.com>
2022-08-08 21:07:36 +0300

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const submitBtn = document.getElementById("submit");
+const form = document.getElementById("form");
+const output = document.getElementById("output");
+
+submitBtn.addEventListener("click", function () {
+  console.log("click");
+});
+
+/* <div id="output" class="output">
+  <img src="./images/illustration-thank-you.svg" />
+  <h3>You selected 4 out of 5</h3>
+  <h1 class="title">Thank you!</h1>
+  <p class="output-desc">
+    We appreciate you taking the time to give a rating. If you ever need
+    more support, don’t hesitate to get in touch!
+  </p>
+</div> */
