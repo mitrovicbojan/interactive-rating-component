14a57b2435a181785ebf613e88d31f6f014ff065
unknown <mitrovicbojan87@gmail.com>
2022-08-18 22:12:21 +0300

    app

--- app.js
+++ app.js
@@ -3,18 +3,14 @@
 const submitBtn = document.getElementById("submit");
 const form = document.getElementById("form");
 const output = document.getElementById("output");
+const spanOutput = document.getElementById("rate");
+const btn1 = document.querySelector("#buttons");
 
 submitBtn.addEventListener("click", function () {
   output.classList.remove("inactive");
   form.classList.add("inactive");
 });
-
-/* <div id="output" class="output">
-  <img src="./images/illustration-thank-you.svg" />
-  <h3>You selected 4 out of 5</h3>
-  <h1 class="title">Thank you!</h1>
-  <p class="output-desc">
-    We appreciate you taking the time to give a rating. If you ever need
-    more support, don’t hesitate to get in touch!
-  </p>
-</div> */
+btn1.addEventListener("click", (e) => {
+  const someClick = e.target.value;
+  spanOutput.textContent = someClick;
+});
